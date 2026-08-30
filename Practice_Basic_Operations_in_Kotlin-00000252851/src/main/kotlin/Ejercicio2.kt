cf0883519b9b770a9412f130d4299641591948ec
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-08-30 13:01:35 -0700

    feat : ejercicio 2

--- Practice_Basic_Operations_in_Kotlin-00000252851/src/main/kotlin/Ejercicio2.kt
+++ Practice_Basic_Operations_in_Kotlin-00000252851/src/main/kotlin/Ejercicio2.kt
@@ -1,4 +1,16 @@
 package edu.itson.jackMurrieta
 
 class Ejercicio2 {
+
+    fun ejercicio2() {
+        var rainbowColor: String = "rojo";
+        println("rainbowColor cambiado : ${rainbowColor}");
+
+        rainbowColor = "violeta";
+        println("rainbowColor cambiado : ${rainbowColor}");
+
+        // val no se puede reasignar
+        val blockColor: String = "negro";
+        println("blockColor : ${blockColor}"); )
+    }
 }
